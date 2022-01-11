cce8cea144200f7f602425203ae22b4a20da1768
Lucy Cai <lucycai@Lucys-MacBook-Pro.local>
2022-01-10 20:56:24 -0500

    testing

--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -25,19 +25,6 @@ const Skeleton = ({ userId, handleLogin, handleLogout }) => {
         />
       )}
       <h1>Good luck on your project :)</h1>
-      <h2> What you need to change in this skeleton</h2>
-      <ul>
-        <li>
-          Change the Frontend CLIENT_ID (Skeleton.js) to your team's CLIENT_ID (obtain this at
-          http://weblab.to/clientid)
-        </li>
-        <li>Change the Server CLIENT_ID to the same CLIENT_ID (auth.js)</li>
-        <li>
-          Change the Database SRV (mongoConnectionURL) for Atlas (server.js). You got this in the
-          MongoDB setup.
-        </li>
-        <li>Change the Database Name for MongoDB to whatever you put in the SRV (server.js)</li>
-      </ul>
       <h2>How to go from this skeleton to our actual app</h2>
       <a href="http://weblab.to/get-started">Check out this getting started guide</a>
     </>
