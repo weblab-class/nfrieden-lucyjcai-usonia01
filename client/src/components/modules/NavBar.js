fa66f612843cc9cd537e79b80c9d88899cbd2a43
nfrieden <95311896+nfrieden@users.noreply.github.com>
2022-01-14 22:14:59 -0500

    login works now

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -1,29 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "@reach/router";
 import "./NavBar.css";
 import Skeleton from "../pages/Skeleton";
+import GoogleLogin, { GoogleLogout } from "react-google-login";
+import { get, post } from "../../utilities";
 
 const GOOGLE_CLIENT_ID = "736706200424-0kf7ogmosqktuuji45om6vavokt7n5m4.apps.googleusercontent.com";
 const NavBar = (props) => {
+
   return (
     <nav className="NavBar-container">
       <div className="Web-title">StoryCollab</div>
       <div className="NavBar-navigations">
         <a href="/" className="NavBar-link">
-          <i class="fas fa-home"></i>
+          <i className="fas fa-home"></i>
         </a>
         <a href="/new_story" className="NavBar-link">
-          <i class="fas fa-plus-square"></i>
+          <i className="fas fa-plus-square"></i>
         </a>
 
         <a href="/active-story" className="NavBar-link">
-          <i class="fas fa-book"></i>
+          <i className="fas fa-book"></i>
         </a>
         <a href="/" className="NavBar-link">
-          <i class="fas fa-bell"></i>
+          <i className="fas fa-bell"></i>
         </a>
-        <Skeleton userId={props.userId} />
+       
+        {/* <Skeleton userId={props.userId} handleLogin={props.handleLogin} handleLogout={props.handleLogout} /> */}
       </div>
+      <span className="Login">
+      {props.userId ? (
+        <GoogleLogout
+          clientId={GOOGLE_CLIENT_ID}
+          buttonText="Logout"
+          onLogoutSuccess={props.handleLogout}
+          onFailure={(err) => console.log(err)}
+        />
+      ) : (
+        <GoogleLogin
+          clientId={GOOGLE_CLIENT_ID}
+          buttonText="Login"
+          onSuccess={props.handleLogin}
+          onFailure={(err) => console.log(err)}
+        />
+      )}
+    </span>
     </nav>
   );
 };
