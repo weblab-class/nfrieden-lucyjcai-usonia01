cc78853bdf26a7a7c98cb6754af59d40f4700f57
Sonia Uwase <uwasonia2001@gmail.com>
2022-01-14 21:56:55 -0500

    word-count

--- client/src/components/pages/Gamepage.js
+++ client/src/components/pages/Gamepage.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import "./GamePage.css";
 
 const GamePage = () => {
+  const [count, setCount] = useState(0);
+
+  const CharCount = (event) => {
+    if (event.target.value.length <= 50) {
+      setCount(event.target.value.length);
+    }
+  };
+
   return (
-    <div>
-      <h1>hi this is the game page</h1>
-    </div>
+    <>
+      <div className="Story-space">
+        <textarea
+          className="item test"
+          placeholder="Your changing story will appear here..."
+          disabled
+        ></textarea>
+
+        <div className="Add">
+          <div className="my-text">
+            <textarea
+              className="item Text-space"
+              onChange={CharCount}
+              placeholder="Type your sentence..."
+              maxLength="50"
+            ></textarea>
+            <span class="Text-space_count"> {count}/50 (Max Character)</span>
+          </div>
+          <input className="item GamePage-addButton" type="button" value="Add!"></input>
+        </div>
+      </div>
+    </>
   );
 };
 
