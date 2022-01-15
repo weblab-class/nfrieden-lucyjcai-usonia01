c53a5013e121850de21b64790d16112cb186eb71
Sonia Uwase <uwasonia2001@gmail.com>
2022-01-14 21:56:13 -0500

    Enter

--- client/src/components/pages/Gamepage.js
+++ client/src/components/pages/Gamepage.js
@@ -1,38 +1,10 @@
-import React, { useState } from "react";
-import "./GamePage.css";
+import React from "react";
 
 const GamePage = () => {
-  const [count, setCount] = useState(0);
-
-  const CharCount = (event) => {
-    if (event.target.value.length <= 50) {
-      setCount(event.target.value.length);
-    }
-  };
-
   return (
-    <>
-      <div className="Story-space">
-        <textarea
-          className="item test"
-          placeholder="Your changing story will appear here..."
-          disabled
-        ></textarea>
-
-        <div className="Add">
-          <div className="my-text">
-            <textarea
-              className="item Text-space"
-              onChange={CharCount}
-              placeholder="Type your sentence..."
-              maxLength="50"
-            ></textarea>
-            <span class="Text-space_count"> {count}/50 (Max Character)</span>
-          </div>
-          <input className="item GamePage-addButton" type="button" value="Add!"></input>
-        </div>
-      </div>
-    </>
+    <div>
+      <h1>hi this is the game page</h1>
+    </div>
   );
 };
 
