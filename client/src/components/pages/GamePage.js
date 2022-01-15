e219e90f73a093cbaa86fe5f8e849784f8b1a81c
lucyjcai <39889792+lucyjcai@users.noreply.github.com>
2022-01-14 21:43:30 -0500

    update GamePage.js

--- client/src/components/pages/GamePage.js
+++ client/src/components/pages/GamePage.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./GamePage.css";
 import SubmitBar from "./SubmitBar.js";
 
 const GamePage = () => {
+  const [count, setCount] = useState(0);
+
+  const CharCount = () => {
+    if (document.getElementsByClassName("Text-space").length <= 50) {
+      setCount(document.getElementsByClassName("Text-space").length);
+    }
+  };
+
   return (
-    <div className="GamePage-container">
-      <h1>hi this is the game page</h1>
-      <SubmitBar />
-    </div>
+    <>
+      <div className="Story-space">
+        <textarea
+          className="item test"
+          placeholder="Your changing story will appear here..."
+        ></textarea>
+
+        <div className="Add">
+          <div className="my-text">
+            <h1>hi this is the game page</h1>
+            <SubmitBar />
+            <textarea
+              className="item Text-space"
+              onChange={(event) => {
+                CharCount();
+              }}
+              placeholder="Type your sentence..."
+            ></textarea>
+            <span class="Text-space_count"> {count}/50 (Max Character)</span>
+          </div>
+          <input className="item GamePage-addButton" type="button" value="Add!"></input>
+        </div>
+      </div>
+    </>
   );
 };
 
