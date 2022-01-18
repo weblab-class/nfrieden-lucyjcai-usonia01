598dd932ad3df2891eb6262285d8e0d87704524e
nfrieden <nfrieden@mit.edu>
2022-01-17 22:38:42 -0500

    Home page buttons

--- client/src/components/pages/Homepage.js
+++ client/src/components/pages/Homepage.js
@@ -1,9 +1,25 @@
 import React from "react";
+import "./Homepage.css";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1> This is the HomePage</h1>
+    <div className="Home-Container" style={{flexDirection: "row", display: "flex"}}>
+      <div style={{flex: 0.5}}>
+          <input
+            className="Home-Options"
+            type="button"
+            value="Create a new story!"
+            // onClick={addNewSentence}
+          ></input>
+        </div>
+        <div style={{flex: 0.5}}>
+          <input
+            className="Home-Options"
+            type="button"
+            value="Join a created story!"
+            // onClick={addNewSentence}
+          ></input>
+        </div>
     </div>
   );
 };
